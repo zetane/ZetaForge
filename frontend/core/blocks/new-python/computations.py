3a09012b0c05525528e012793a79b99acd39908d
François LaBerge <francois@zetane.com>
2024-03-22 23:15:25 +0000

    Remove the New_Python block

--- frontend/core/blocks/new-python/computations.py
+++ frontend/core/blocks/new-python/computations.py
@@ -1,11 +1,9 @@
-# Python block template
-def compute(in1, in2, in3):
+def compute(in1, in2):
     """A textual description of the compute function.
 
     Inputs:
         in1 (all): Textual description of in1
         in2 (all): Textual description of in2
-        in3 (all): Textual description of in2
 
     Outputs:
         out1 (all): Textual description of out1
@@ -13,8 +11,9 @@ def compute(in1, in2, in3):
 
     Requirements:
     """
-
-    # Your code
+    # some code
+    out1 = 2 * in1
+    out2 = "This is the in2 string:" + in2
 
     return {"out1": out1, "out2": out2}
 
