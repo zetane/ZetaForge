0df5db450c6db3f302c29044cbdd2d0c973b9010
François LaBerge <35940434+FGRCL@users.noreply.github.com>
2024-04-17 17:05:03 -0400

    center the blocks when loaded (#25)

--- frontend/src/components/ui/LoadBlockButton.jsx
+++ frontend/src/components/ui/LoadBlockButton.jsx
@@ -1,17 +1,19 @@
-import { HeaderMenuItem } from "@carbon/react";
-import { useRef } from "react";
-import { pipelineAtom, getPipelineFormat } from "@/atoms/pipelineAtom";
-import { customAlphabet } from "nanoid";
-import { useImmerAtom } from 'jotai-immer'
-import { useAtom } from "jotai";
-import { trpc } from "@/utils/trpc"
 import { getDirectoryPath } from "@/../utils/fileUtils";
+import { drawflowEditorAtom } from "@/atoms/drawflowAtom";
 import { mixpanelAtom } from "@/atoms/mixpanelAtom";
+import { getPipelineFormat, pipelineAtom } from "@/atoms/pipelineAtom";
+import { trpc } from "@/utils/trpc";
+import { HeaderMenuItem } from "@carbon/react";
+import { useAtom } from "jotai";
+import { useImmerAtom } from 'jotai-immer';
+import { customAlphabet } from "nanoid";
+import { useRef } from "react";
 
 export default function LoadBlockButton() {
   const FILE_EXTENSION_REGEX = /\.[^/.]+$/;
   const [pipeline, setPipeline] = useImmerAtom(pipelineAtom)
   const [mixpanelService] = useAtom(mixpanelAtom)
+  const [editor] = useAtom(drawflowEditorAtom)
 
   const fileInput = useRef();
 
@@ -26,6 +28,7 @@ export default function LoadBlockButton() {
     const newNanoid = nanoid()
     const id = `${block.information.id}-${newNanoid}`
 
+    block = centerBlockPosition(block)
     const newPipeline = getPipelineFormat(pipeline)
     let newPipelineData = JSON.parse(JSON.stringify(pipeline.data))
     newPipelineData[id] = block
@@ -52,8 +55,8 @@ export default function LoadBlockButton() {
   const loadBlock = async (pipeline) => {
     try {
       mixpanelService.trackEvent('Load Block')
-    } catch(err){
-        
+    } catch (err) {
+
     }
     const files = fileInput.current.files
     for (const key in files) {
@@ -65,7 +68,7 @@ export default function LoadBlockButton() {
         const spec = JSON.parse(await (new Blob([file])).text())
         let folderPath = getDirectoryPath(file.path)
         folderPath = folderPath.replaceAll('\\', '/')
-        
+
         await addBlockToPipeline(spec, folderPath)
 
         fileInput.current.value = ''
@@ -73,6 +76,12 @@ export default function LoadBlockButton() {
       }
     }
   };
+
+  const centerBlockPosition = (block) => {
+    block.views.node.pos_x = ((editor.precanvas.clientWidth / 2) - editor.precanvas.getBoundingClientRect().x) / editor.zoom;
+    block.views.node.pos_y = ((editor.precanvas.clientHeight / 2) - editor.precanvas.getBoundingClientRect().y) / editor.zoom;
+    return block;
+  }
 
   const removeFileExtension = (fileName) => {
     return fileName.replace(FILE_EXTENSION_REGEX, "");
