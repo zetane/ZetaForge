ce15f4e9446d3d3f1eff8bcb40de2d43d1f3bd0d
plparent <pierre-luc@zetane.com>
2024-05-14 13:41:45 -0400

    Add modal

--- frontend/src/components/ui/GetEndpointButton.jsx
+++ frontend/src/components/ui/GetEndpointButton.jsx
@@ -3,9 +3,13 @@ import axios from "axios";
 import { pipelineAtom } from "@/atoms/pipelineAtom";
 import { useMutation } from "@tanstack/react-query";
 import { Button } from "@carbon/react";
+import ClosableModal from "./modal/ClosableModal";
+import { useState } from "react";
 
 export default function GetEndpointButton({modalPopper, children, action}) {
     const [pipeline, setPipeline] = useImmerAtom(pipelineAtom);
+    const [validationErrorMsg, setValidationErrorMsg] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
 
     const styles = {
         margin: '5px',
@@ -21,13 +25,20 @@ export default function GetEndpointButton({modalPopper, children, action}) {
         const res = await mutation.mutateAsync(pipeline.id)
         if (res.status === 200) {
             if (res.data !== null) {
-                console.log(`${import.meta.env.VITE_EXECUTOR}/pipeline/${pipeline.id}/${res.data[0].Hash}/execute`)
+                setValidationErrorMsg(prev => {
+                    return [`${import.meta.env.VITE_EXECUTOR}/pipeline/${pipeline.id}/${res.data[0].Hash}/execute`];
+                })
             } else {
-                console.log("This pipeline has not been deployed")
+                setValidationErrorMsg(prev => {
+                    return "This pipeline has not been deployed";
+                })
             }
         } else {
-            console.log("An error occured.")
+            setValidationErrorMsg(prev => {
+                return res.data;
+            })
         }
+        setIsOpen(true)
     }
 
     return (
