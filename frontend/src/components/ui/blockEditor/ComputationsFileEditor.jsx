f838fe558b95e1f2f66943c00e32f942c11ff655
François LaBerge <fgrclaberge@gmail.com>
2024-05-15 09:43:58 -0400

    remove comments in ComputationsFileEditor

--- frontend/src/components/ui/blockEditor/ComputationsFileEditor.jsx
+++ frontend/src/components/ui/blockEditor/ComputationsFileEditor.jsx
@@ -28,13 +28,12 @@ import { EditorCodeMirror, ViewerCodeMirror } from "./CodeMirrorComponents";
 export default function ComputationsFileEditor({ fetchFileSystem }) {
   const serverAddress = "http://localhost:3330";
   const [blockPath] = useAtom(blockEditorRootAtom);
-  const [blockFolderName, setBlockFolderName] = useState(null); //TODO check if still needed
+  const [blockFolderName, setBlockFolderName] = useState(null);  
   const [openAIApiKey] = useAtom(openAIApiKeyAtom);
   const [pipeline, setPipeline] = useImmerAtom(pipelineAtom);
   const [editor] = useAtom(drawflowEditorAtom);
   const [compilationErrorToast, setCompilationErrorToast] = useAtom(compilationErrorToastAtom)
 
-  // const [agentName, setAgentName] = useState('gpt-4_python_compute');
   const [agentName, setAgent] = useState("gpt-4_python_compute");
 
   const [queryAndResponses, setQueryAndResponses] = useState([]);
@@ -101,18 +100,6 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
       }
     };
 
-    // const fetchChatHistoryIndex = async () => {
-    //   try {
-    //     const response = await fetch(`${serverAddress}/get-chat-history-index?blockPath=${encodeURIComponent(blockPath)}`);
-    //     if (!response.ok) {
-    //       throw new Error('Failed to fetch chat history index');
-    //     }
-    //     const data = await response.json();
-    //     setActiveCodeMirror(data.chatHistoryIndex);
-    //   } catch (error) {
-    //     console.error("Error fetching chat history index:", error);
-    //   }
-    // };
     const fetchChatHistoryIndex = async () => {
       try {
         const response = await fetch(`${serverAddress}/get-chat-history-index?blockPath=${encodeURIComponent(blockPath)}`);
@@ -123,7 +110,6 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
         if (data.chatHistoryIndex !== -1) {
           setActiveCodeMirror(data.chatHistoryIndex);
         } else {
-          // Handle the case where no index is found, maybe initialize to the first position
           setActiveCodeMirror(0);
         }
       } catch (error) {
@@ -137,14 +123,6 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
     fetchChatHistoryIndex();
   }, [blockPath]);
 
-//   useEffect(() => {
-//     // Hack. CodeMirror loads code asynchronously and doesn't assume its full size until it is done loading.
-//     // There isn't a simple way to run code when CodeMirror is done loading. 
-//     // We add a delay so that the scrolling happens when CodeMirror is at its full size. 
-//     setTimeout(() => {
-//       panel.current.scrollTo({ lef: 0, top: panel.current.scrollHeight, behavior: "smooth" })
-//     }, 100)
-//   }, [queryAndResponses, showEditor])
 
   const recordCode = (promptToRecord, codeToRecord) => {
     const newQueriesAndResponses = [
@@ -213,28 +191,24 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
       recordCode(editorManualPrompt, editorValue);
     }
     setShowEditor(false);
-        // Scroll to the bottom of the component
         if (panel.current) {
             setTimeout(() => {
                 panel.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
             }, 100);
         }
-    // e.currentTarget.blur();
   };
 
   const handleEdit = (e) => {
-    // Extract the index from the button's ID
     const buttonId = e.currentTarget.id;
     const index = parseInt(buttonId.split("-").pop(), 10);
 
     if (index >= 0 && index < queryAndResponses.length) {
       setEditorValue(queryAndResponses[index].response);
       setEditorManualPrompt("Manual edit of code #" + index);
-      setShowEditor(true); // Show the EditorCodeMirror
+      setShowEditor(true);
     }
     e.currentTarget.blur();
 
-    // Scroll to the bottom of the component
     if (panel.current) {
         setTimeout(() => {
             panel.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
@@ -242,81 +216,7 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
         }
   };
 
-
-
-  // const fetchFileSystem = useCallback(async () => {
-  //   try {
-  //     const response = await fetch(`${serverAddress}/get-directory-tree`, {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ folder: blockPath }),
-  //     });
-  //     if (!response.ok) {
-  //       throw new Error(`HTTP error! status: ${response.status}`);
-  //     }
-
-  //     const data = await response.json();
-  //     const inserted_data = {
-  //       [blockFolderName]: { content: data, expanded: true, type: "folder" },
-  //     };
-  //     setFileSystem(inserted_data);
-  //   } catch (error) {
-  //     console.error("Error fetching file system:", error);
-  //   }
-  // }, [blockFolderName, blockPath]);
-
-
-  // const handleGenerate = async (e, index) => {
-  //   e.currentTarget.blur();
-
-  //   setLastGeneratedIndex(index);
-  //   let code_content = editorValue;
-
-  //   const requestBody = JSON.stringify({
-  //     block_user_name: blockFolderName,
-  //     block_name: blockFolderName,
-  //     agent_name: agentName,
-  //     blockPath: blockPath,
-  //     computations_script: code_content,
-  //   });
-
-  //   try {
-  //     const response = await fetch(`${serverAddress}/new-block-react`, {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: requestBody,
-  //     });
-
-  //     if (!response.ok) {
-  //       throw new Error(`HTTP error! status: ${response.status}`);
-  //     }
-  //   } catch (error) {
-  //     console.error("Request failed: " + error.message);
-  //   }
-
-  //   try {
-  //     const newSpecsIO = await compileComputation.mutateAsync({ blockPath: blockPath });
-  //     const newSpecs = await updateSpecs(blockFolderName, newSpecsIO, pipeline.data, editor);
-  //     setPipeline((draft) => {
-  //       draft.data[blockFolderName] = newSpecs;
-  //     })
-  //     await saveBlockSpecs.mutateAsync({ blockPath: blockPath, blockSpecs: newSpecs });
-  //   } catch (error) {
-  //     console.error(error)
-  //     setCompilationErrorToast(true);
-  //   }
-
-  //   setActiveCodeMirror(index);
-
-  //   fetchFileSystem(blockFolderName);
-  // };
-  // const handleGenerate = async (e, index) => {
-    const handleGenerate = async (index) => {
-    // e.currentTarget.blur();
+  const handleGenerate = async (index) => {
 
     setLastGeneratedIndex(index);
     let code_content = queryAndResponses[index].response;
@@ -359,8 +259,7 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
     }
 
     setActiveCodeMirror(index);
-    
-    // fetchFileSystem(blockFolderName);
+
     fetchFileSystem();
   };
 
@@ -382,7 +281,6 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
       console.error("Request failed: " + error.message);
     }
 
-    // fetchFileSystem(blockFolderName);
     fetchFileSystem();
   }, [blockFolderName, blockPath, fetchFileSystem]);
 
@@ -390,13 +288,6 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
     await handleGenerate(e, index);
     await handleDockerCommands();
   };
-
-//   const handleSaveCompile = async (event) => {
-//     event.preventDefault(); // Prevent the default form submission behavior
-//     await handleSave();     // Assuming handleSave knows what to save
-//     handleGenerate(event);  // Assuming handleGenerate does not need an index
-//     onUpdateDirectoryViewer();
-// };
 
 
   return (
@@ -407,12 +298,9 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
             {item.prompt}
             </span>
             <div>
-            <div className="flex items-center mb-4"> {/* Flex container for horizontal alignment */}
-              {/* <div className='bg-[var(--ztn-purple-0)] py-2 pl-2 rounded'> */}
-
+              <div className="flex items-center mb-4">
                 <RadioButton
-                    id={`select-button-${index}`}
-                    labelText=""  // Hide label text
+                  id={`select-button-${index}`}
                     name="activeCodeMirrorSelection"
                     value={index.toString()}
                     checked={activeCodeMirror === index}
@@ -441,35 +329,14 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
                     tooltipPosition="top"
                     hasIconOnly
                     size="md"
-                    // kind="ghost"
                     onClick={handleEdit}
-                />
-  
-                {/* <Button
-                    id={`generate-button-${index}`}
-                    renderIcon={OperationsRecord}
-                    iconDescription="Select Active Code"
-                    hasIconOnly
-                    size="md"
-                    kind="ghost"
-                    onClick={(e) => handleGenerate(e, index)}
-                /> */}
-                {/* <Button
-                    id={`execute-button-${index}`}
-                    renderIcon={Run}
-                    iconDescription="Compile files and run block test"
-                    hasIconOnly
-                    size="md"
-                    kind="ghost"
-                    onClick={(e) => handleSequentialExecution(e, index)}
-                /> */}
+                  />
                 </div>
             </div>
             </div>
         </Fragment>
         ))}
-        {showEditor ? (
-        // Render EditorCodeMirror if showEditor is true
+      {showEditor ? (
         <div>
             <div className='block-editor-code-header'>{editorManualPrompt}</div>
             <div className="relative">
@@ -484,34 +351,16 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
                 tooltipPosition="left"
                 hasIconOnly
                 size="md"
-                // kind="ghost"
                 className="edit-button"
                 onClick={handleSave}
-                // onClick={handleSaveCompile}
                 />
             </div>
             </div>
         </div>
-        ) : (
-        // Render input group if showEditor is false
+      ) : (
         <>
             {openAIApiKey &&
-            <div className="relative">
-                {/* <div className="p-4">
-                  <Bot size={24} className="mx-0 align-middle" />
-                  <span className="text-md">{agentName}</span>
-                </div>
-                <textarea
-                className="block-editor-prompt-input"
-                ref={chatTextarea}
-                placeholder="Ask to generate code or modify last code"
-                onKeyDown={(e) => {
-                    if (e.key === "Enter" && !e.shiftKey) {
-                    e.preventDefault();
-                    handleSubmit(e);
-                    }
-                }}
-                /> */}
+              <div className="relative">
                 <div className="text-right">
                     <div className="inline-block p-2">
                         <Bot size={24} className="align-middle" />
@@ -535,10 +384,7 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
                       <div className='prompt-spinner'>
                         <Loading active={true} className="send-spinner" description="Sending..." withOverlay={false}/>
                         </div>
-                    ) : (
-                        // <IconButton iconDescription="Send" tooltipPosition="left" size="md" kind="ghost" onClick={handleSubmit}>
-                        //     <Send size={24} />
-                        // </IconButton>
+                  ) : (
                         <button className="icon-button" onClick={handleSubmit} title="Submit">
                         <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M2 21L23 12L2 3V10L17 12L2 14V21Z" fill="currentColor"/>
@@ -550,7 +396,6 @@ export default function ComputationsFileEditor({ fetchFileSystem }) {
 
             </div>
             }
-            
         </>
         )}
     </div>
