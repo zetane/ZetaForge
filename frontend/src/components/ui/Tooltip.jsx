9b6d0c297392e43f81e704f251f3f23572908ab6
François LaBerge <fgrclaberge@gmail.com>
2024-05-23 14:50:10 -0400

    remove floating ui

--- frontend/src/components/ui/Tooltip.jsx
+++ frontend/src/components/ui/Tooltip.jsx
@@ -1,44 +1,15 @@
-import { flip, offset, shift, useFloating, useFocus, useHover, useInteractions, useRole } from "@floating-ui/react";
-import { useState } from "react";
 
 export default function Tooltip({ children, label }) {
-  const [isOpen, setIsOpen] = useState(true);
 
-  const { refs, floatingStyles, context } = useFloating({
-    open: isOpen,
-    onOpenChange: setIsOpen,
-    middleware: [offset(), flip(), shift()]
-  })
-
-  const hover = useHover(context, {
-    move: false
-  });
-  const focus = useFocus(context);
-  const role = useRole(context, {
-    role: "tooltip",
-    label: "label"
-  })
-
-  const { getReferenceProps, getFloatingProps } = useInteractions([
-    hover,
-    focus,
-    role
-  ])
-
-  console.log(floatingStyles)
-  console.log(getFloatingProps())
-  return <div ref={refs.setReference} {...getReferenceProps()}>
-    {children}
-    {isOpen &&
+  return (
+    <div className="group">
+      {children}
       <span
-        ref={refs.setFloating}
-        style={floatingStyles}
-        {...getFloatingProps()}
-        className="absolute tooltip-text">
-        <span className="fixd tooltip-text">
+        className="absolute invisible group-hover:visible -bottom-2 left-0">
+        <span className="fixed tooltip">
           {label}
         </span>
       </span>
-    }
-  </div >
+    </div >
+  )
 }
