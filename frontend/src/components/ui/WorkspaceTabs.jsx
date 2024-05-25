df90f225a2d26ea5d736845ef25c0b90c0bff4dd
Jon Magoon <js.magoon@gmail.com>
2024-05-24 18:16:04 -0600

    logs

--- frontend/src/components/ui/WorkspaceTabs.jsx
+++ frontend/src/components/ui/WorkspaceTabs.jsx
@@ -10,15 +10,11 @@ export default function WorkspaceTabs() {
 
   useEffect(() => {
     const pipelineTabs = Object.values(workspace.tabs).map((key) => {
-      console.log("key: ", key)
       const pipeline = workspace.pipelines[key]
-      console.log(pipeline)
-      console.log(workspace)
       if (pipeline) {
-        return {label: pipeline?.name, panel: <TabPanel/>}
+        return {id: pipeline.id, label: pipeline?.name, panel: <TabPanel/>}
       }
     })
-    console.log("tabs: ", pipelineTabs)
     setRenderedTabs(pipelineTabs)
     const pipelineKeys = Object.keys(workspace.pipelines)
     for (let i = 0; i < pipelineKeys.length; i++) {
@@ -44,6 +40,20 @@ export default function WorkspaceTabs() {
 
     const selectedTab = renderedTabs[selectedIndex];
 
+    // TODO: Pop modal for deleting last tab
+    if (workspace.tabs.length > 1) {
+      const pipeline = workspace.pipelines[selectedTab.id]
+
+      setWorkspace((draft) => {
+        const filteredTabs = workspace.tabs.filter((tab) => selectedTab.id == tab.id)
+        draft.tabs = filteredTabs
+        // If it's an empty pipeline we can just delete it
+        if (Object.keys(pipeline.data).length === 0 && !pipeline.record) {
+          delete draft.pipelines[pipeline.id]
+        }
+      })
+    }
+
     const filteredTabs = renderedTabs.filter((_, index) => index !== tabIndex);
     // if the tab being deleted is the currently selected tab, we're re-setting the selectedIndex
     // to the first tab available that isn't disabled
@@ -55,7 +65,6 @@ export default function WorkspaceTabs() {
       // due to a tab element being removed from the array
       setSelectedIndex(filteredTabs.indexOf(selectedTab));
     }
-    setRenderedTabs(filteredTabs);
   };
   return (
     <div className="tab-bar flex flex-wrap">
