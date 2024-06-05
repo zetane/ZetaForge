8c6d7f85d071a78b19240ac5c8f4d1c48568aa28
François LaBerge <fgrclaberge@gmail.com>
2024-06-05 13:45:32 -0400

    configure ignore files

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,10 +3,19 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  {
+    ignores: [
+      ".cache/*",
+      "core/*",
+      "dist/*",
+      "dist-electron/*",
+      "electron/*",
+      "release/*",
+    ],
+  },
 ];
