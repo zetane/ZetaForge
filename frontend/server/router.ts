7721ca2b56bab327a60c7b8352acefc243ea445d
François LaBerge <fgrclaberge@gmail.com>
2024-07-04 11:19:01 -0400

    add the certiticate to the router

--- frontend/server/router.ts
+++ frontend/server/router.ts
@@ -228,6 +228,7 @@ export const appRouter = router({
           anvil: z.object({
             host: z.string(),
             port: z.string(),
+            certificate: z.string(),
           }),
           s3: z.object({
             host: z.string(),
