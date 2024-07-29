74d0d4f36a955f0972a5b2aef7f8c578c5ed8461
Jon Magoon <js.magoon@gmail.com>
2024-07-29 10:51:38 -0600

    remove log line

--- executor.go
+++ executor.go
@@ -811,7 +811,6 @@ func localExecute(pipeline *zjson.Pipeline, executionId int64, executionUuid str
 	}
 
 	workflow, err = runArgo(ctx, workflow, executionId, cfg, db, hub)
-	log.Printf("Name: %v", workflow.Name)
 	if workflow != nil {
 		defer deleteArgo(ctx, workflow.Name, cfg)
 	}
