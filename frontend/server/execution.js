048594cf2bb3fe1665fd164eef1c85521d37464d
François LaBerge <fgrclaberge@gmail.com>
2024-07-31 16:49:33 -0400

    Add the org when to the s3 key when syncing results

--- frontend/server/execution.js
+++ frontend/server/execution.js
@@ -7,7 +7,15 @@ export async function syncExecutionResults(
   executionUuid,
   anvilConfiguration,
 ) {
-  const s3Prefix = `${pipelineUuid}/${executionUuid}`;
+  let s3Prefix;
+  if (anvilConfiguration.anvil.token) {
+    const data = atob(anvilConfiguration.anvil.token.split(".")[1]);
+    const org = JSON.parse(data).sub;
+    s3Prefix = `${org}/${pipelineUuid}/${executionUuid}`;
+  } else {
+    s3Prefix = `${pipelineUuid}/${executionUuid}`;
+  }
+
   const localPath = path.join(buffer, "history", executionUuid, "files");
 
   await syncS3ToLocalDirectory(s3Prefix, localPath, anvilConfiguration);
