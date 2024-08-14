89d1917301b25ecaf530d62bb3a45484b2cb64fb
François LaBerge <fgrclaberge@gmail.com>
2024-08-14 13:58:10 -0400

    remove modal

--- frontend/src/components/ui/blockEditor/directoryViewer/FileViewer.jsx
+++ frontend/src/components/ui/blockEditor/directoryViewer/FileViewer.jsx
@@ -2,7 +2,6 @@ import {
   BLOCK_SPECS_FILE_NAME,
   CHAT_HISTORY_FILE_NAME,
 } from "@/utils/constants";
-import { useState } from "react";
 import CodeViewer from "./CodeViewer";
 import CodeEditor from "./CodeEditor";
 
@@ -14,35 +13,6 @@ export default function FileViewer({
   promptResponse,
   onAcceptPrompt,
 }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [pendingFile, setPendingFile] = useState(null);
-
-  const handleModalConfirm = (e) => {
-    // saveChanges(e);
-    // if (pendingFile) {
-    //   const relPath = pendingFile.replaceAll("\\", "/");
-    //   const pathSegments = relPath.split("/");
-    //   let fileContent = fileSystem;
-    //
-    //   for (let i = 0; i < pathSegments.length; i++) {
-    //     const segment = pathSegments[i];
-    //     if (i === pathSegments.length - 1) {
-    //       fileContent = fileContent[segment].content;
-    //     } else {
-    //       fileContent = fileContent[segment].content;
-    //     }
-    //   }
-    //
-    //   setCurrentFile({ path: pendingFile, content: fileContent });
-    // }
-    // setIsModalOpen(false);
-    // setPendingFile(null);
-  };
-
-  const handleModalCancel = () => {
-    setIsModalOpen(false);
-  };
-
   const getViewer = () => {
     if (currentFile) {
       const isReadOnly = READ_ONLY_FILES.some((fileName) =>
@@ -72,26 +42,4 @@ export default function FileViewer({
   };
 
   return getViewer();
-
-  // return (
-  //   <>
-  //         {if (isPrompt) <CodeViewer
-  //             fileContentBuffer={fileContentBuffer}
-  //           />
-  //         else if (isReadOnly)
-  //         elsvkke
-  //         }
-  //     <Modal
-  //       open={isModalOpen}
-  //       modalHeading="Unsaved Changes"
-  //       primaryButtonText="Save Changes"
-  //       secondaryButtonText="Cancel"
-  //       onRequestSubmit={handleModalConfirm}
-  //       onRequestClose={handleModalCancel}
-  //       size="xs"
-  //     >
-  //       <p>You have unsaved changes. Do you want to save them?</p>
-  //     </Modal>
-  //   </>
-  // );
 }
