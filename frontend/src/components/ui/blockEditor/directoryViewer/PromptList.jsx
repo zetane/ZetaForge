b98cbbd71b236d1dd659cfdd7820752c3996558d
François LaBerge <fgrclaberge@gmail.com>
2024-08-15 15:32:18 -0400

    make prompt list scrollable

--- frontend/src/components/ui/blockEditor/directoryViewer/PromptList.jsx
+++ frontend/src/components/ui/blockEditor/directoryViewer/PromptList.jsx
@@ -10,9 +10,9 @@ export default function PromptList({ pipelineId, blockId, onSelectPrompt}) {
 
   // TODO make the list scrollable
   return (
-    <div className="p-3">
-      <div className="pb-5">Prompts ({historyData.length})</div>
-      <div className="flex flex-col gap-2">
+    <div className="p-3 flex flex-col max-h-full gap-5">
+      <div>Prompts ({historyData.length})</div>
+      <div className="flex flex-col gap-2 pr-1 flex-1 min-h-0 overflow-auto">
         {historyData.map(({ prompt, response }, index) => (
           <Prompt key={index} index={index} pipelineId={pipelineId} blockId={blockId} response={response} onSelectPrompt={onSelectPrompt}>{prompt}</Prompt>
         ))}
