ea8ab7e8724b275a243cd476aa49f5c1eccdde75
François LaBerge <fgrclaberge@gmail.com>
2024-08-19 14:15:01 -0400

    add check for the persitent allotment storage keys

--- frontend/src/components/ui/PersistentAllotment.jsx
+++ frontend/src/components/ui/PersistentAllotment.jsx
@@ -2,8 +2,14 @@ import { Allotment } from "allotment";
 import { allotmentSizesAtom } from "@/atoms/allotmentAtom";
 import { useImmerAtom } from "jotai-immer";
 
+const storageKeys = {};
+
 export default function PersistentAllotment({ storageKey, initialSize, children, ...rest }) {
-  //TODO find a way to get a generated storage key or test for uniqueness
+  if (storageKeys[storageKey]) {
+    console.warn(`PersistentAllotment' storageKey "${storageKey}" has already been used by another component. Did you forget to change the stoageKey?.`);
+  }
+  storageKeys[storageKey] = true;
+
   const [allotmentSizes, setAllotmentSizes] = useImmerAtom(allotmentSizesAtom);
   const size = allotmentSizes[storageKey] ?? initialSize;
 
