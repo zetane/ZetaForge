f6f8aeccdbf3c6d068935cb52cf7df8ea03a4bdf
François LaBerge <fgrclaberge@gmail.com>
2024-08-21 13:06:45 -0400

    Display code viewer properly

--- frontend/src/components/ui/blockEditor/directoryViewer/CodeViewer.jsx
+++ frontend/src/components/ui/blockEditor/directoryViewer/CodeViewer.jsx
@@ -1,12 +1,22 @@
+import { useState, useEffect } from "react";
 import { trpc } from "@/utils/trpc";
 import { ViewerCodeMirror } from "./CodeMirrorComponents";
 
 export default function CodeViewer({ pipelineId, blockId, currentFile }) {
-  const fileContent = trpc.block.file.byPath.get.useQuery({
-    pipelineId: pipelineId,
-    blockId: blockId,
-    path: currentFile.relativePath,
-  });
+  const getFileContent = trpc.block.file.byPath.get.useMutation();
+  const [fileContentBuffer, setFileContentBuffer] = useState();
 
-  return <ViewerCodeMirror code={fileContent.data ?? ""} />
+  useEffect(() => {
+    const fetchFileContent = async () => {
+      const content = await getFileContent.mutateAsync({
+        pipelineId: pipelineId,
+        blockId: blockId,
+        path: currentFile.relativePath,
+      });
+      setFileContentBuffer(content);
+    };
+
+    fetchFileContent();
+  }, []);
+  return <ViewerCodeMirror code={fileContentBuffer ?? ""} />
 }
