54c8c018a3776c5a35e0adcee9da89946470cadd
François LaBerge <fgrclaberge@gmail.com>
2024-08-26 14:50:20 -0400

    remove empty line

--- frontend/src/components/ui/blockEditor/directoryViewer/CodeEditor.jsx
+++ frontend/src/components/ui/blockEditor/directoryViewer/CodeEditor.jsx
@@ -6,7 +6,6 @@ import { SelectedPromptContext } from "./SelectedPromptContext";
 export default function CodeEditor() {
   const selectedPrompt = useContext(SelectedPromptContext);
 
-
   return (
     <div className="flex h-full flex-col gap-8">
       {selectedPrompt.selectedPrompt ? <PromptViewer /> : <CodeManualEditor />}
