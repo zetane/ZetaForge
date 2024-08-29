40a9f0437e56eefb75b0488d0d5775b6d8dcef12
François LaBerge <fgrclaberge@gmail.com>
2024-08-29 12:16:55 -0400

    move the tabs in the header

--- frontend/src/components/ui/blockEditor/Editor.jsx
+++ frontend/src/components/ui/blockEditor/Editor.jsx
@@ -19,7 +19,7 @@ export default function Editor({ blockId, isMaximized, onToggleMaximize }) {
   const blockName = pipeline.data[blockId].information.name;
   const setBlockEditorOpen = useSetAtom(isBlockEditorOpenAtom);
 
-  const sizeStyle =  isMaximized ? "maximized" : "minimized";
+  const sizeStyle = isMaximized ? "maximized" : "minimized";
 
   const handleClose = () => {
     setBlockEditorOpen(false);
@@ -31,57 +31,45 @@ export default function Editor({ blockId, isMaximized, onToggleMaximize }) {
 
   return (
     <div
-      className={
-        `editor-block absolute z-[8000] flex flex-col ${sizeStyle}`
-      }
+      className={`editor-block absolute z-[8000] flex flex-col ${sizeStyle}`}
     >
-      <div className="block-editor-header">
-        <div className="py-2 pl-4">
-          <p className="text-base italic">{blockName}</p>
-          <p className="text-xs">{blockId}</p>
-        </div>
-        <div className="flex flex-row items-center justify-end">
-          <IconButton
-            kind="ghost"
-            onClick={toggleMaximize}
-            label={isMaximized ? "Minimize" : "Maximize"}
-          >
-            {isMaximized ? <Minimize/> : <Maximize/>}
-          </IconButton>
-          <IconButton
-            kind="ghost"
-            onClick={handleClose}
-            label="Close"
-          >
-            <Close/>
-          </IconButton>
-        </div>
-      </div>
       <Tabs>
-        <TabList
-          fullWidth
-          className="mx-auto max-w-[40rem] shrink-0"
-          aria-label="Editor tabs mb-1"
-        >
-          <Tab>Files</Tab>
-          <Tab>Specs</Tab>
-          <Tab>Test Block</Tab>
-        </TabList>
+        <div className="block-editor-header">
+          <div className="py-2 pl-4">
+            <p className="text-base italic">{blockName}</p>
+            <p className="text-xs">{blockId}</p>
+          </div>
+          <TabList
+            fullWidth
+            className="max-w-[40rem]"
+            aria-label="Editor tabs mb-1"
+          >
+            <Tab>Files</Tab>
+            <Tab>Specs</Tab>
+            <Tab>Test Block</Tab>
+          </TabList>
+          <div className="flex flex-row items-center justify-end">
+            <IconButton
+              kind="ghost"
+              onClick={toggleMaximize}
+              label={isMaximized ? "Minimize" : "Maximize"}
+            >
+              {isMaximized ? <Minimize /> : <Maximize />}
+            </IconButton>
+            <IconButton kind="ghost" onClick={handleClose} label="Close">
+              <Close />
+            </IconButton>
+          </div>
+        </div>
         <TabPanels>
-          <TabPanel className="overflow-hidden h-full p-0">
+          <TabPanel className="h-full overflow-hidden p-0">
             <DirectoryViewer key={blockId} blockId={blockId} />
           </TabPanel>
           <TabPanel className="overflow-y-auto">
-            <SpecsInterface
-              key={blockId}
-              blockId={blockId}
-            />
+            <SpecsInterface key={blockId} blockId={blockId} />
           </TabPanel>
           <TabPanel className="overflow-hidden">
-            <TestLogs
-              pipelineId={pipeline.id}
-              blockId={blockId}
-            />
+            <TestLogs pipelineId={pipeline.id} blockId={blockId} />
           </TabPanel>
         </TabPanels>
       </Tabs>
