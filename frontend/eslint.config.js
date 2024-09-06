6bedc2a19e214216786d22fd1921bb3d84916676
François LaBerge <fgrclaberge@gmail.com>
2024-09-06 13:51:55 -0400

    use the proper eslint config for react

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,13 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import reactPlugin from "eslint-plugin-react";
 
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig,
+  reactPlugin.configs.flat["jsx-runtime"],
   {
     files: [
       "**/*.js",
