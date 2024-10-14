d0a02ed5d4698a91ca102d1de73a901fe82a79c1
François LaBerge <35940434+FGRCL@users.noreply.github.com>
2024-10-14 11:31:25 -0400

    Handle a 403 Error from S3 when users don't have full access to the S3 bucket (#223)
    * handle the 403 errors properly
    
    * remove logs

--- frontend/server/s3.js
+++ frontend/server/s3.js
@@ -123,10 +123,9 @@ async function fileExists(key, anvilConfiguration) {
     );
     return true;
   } catch (err) {
-    if (err.name === "NotFound") {
+    if (err.name === "NotFound" || err.name === "403") {
       return false;
     }
-    console.log(err);
     const message = "Error checking file existence in S3";
     logger.error(err, message);
     throw new Error(message);
