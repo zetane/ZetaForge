036b0899ef95c75f0b25f81ed66d59a5f3669231
Maharaj Teertha Deb <86840917+TeerthaDeb@users.noreply.github.com>
2024-10-23 11:28:18 -0400

    added Tabs and TabPanels from @carbon/react

--- frontend/src/components/ui/DeployedPipelineActions.jsx
+++ frontend/src/components/ui/DeployedPipelineActions.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
-import { ComboButton, MenuItem, CodeSnippet } from "@carbon/react";
-import { Application, TrashCan } from "@carbon/icons-react";
-import { modalContentAtom } from "@/atoms/modalAtom";
-import { useAtom } from "jotai";
+import {
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  ComboButton,
+  CodeSnippet,
+} from "@carbon/react";
 import ClosableModal from "./modal/ClosableModal";
 import { getScheme } from "@/client/anvil";
 
@@ -43,8 +48,7 @@ export const DeployedPipelineActions = ({
       }
     });
 
-    const pythonCode = `
-from zetaforge import Zetaforge
+    const pythonCode = `from zetaforge import Zetaforge
 
 zetaforge = Zetaforge(address='${getScheme(configuration.anvil.host)}://${configuration.anvil.host}:${configuration.anvil.port}', token='${configuration.anvil.token}')
 
@@ -52,16 +56,15 @@ result = zetaforge.run('${name}:${hash.substring(0, 8)}', input=${JSON.stringify
 print('Pipeline execution result:', result)
 `;
 
-    const jsCode = `
-import Zetaforge from "zetaforge";
+    const jsCode = `import Zetaforge from "zetaforge";
 
 const zetaforge = new Zetaforge({
   '${getScheme(configuration.anvil.host)}://${configuration.anvil.host}:${configuration.anvil.port}',
   '${configuration.anvil.token}'
 });
 
-const pipelineUuid = ${uuid};
-const pipelineHash = ${hash};
+const pipelineUuid = "${uuid}";
+const pipelineHash = "${hash}";
 const inputs = ${JSON.stringify(inputs, null, 2)};
 
 async function executePipeline() {
@@ -87,14 +90,11 @@ executePipeline(); // it will execute the pipeline....
 
   return (
     <>
-      <ComboButton size="sm" label="Payload" onClick={generatePostPayload}>
-        <MenuItem
-          label="Undeploy"
-          onClick={handleUndeploy}
-          renderIcon={TrashCan}
-          kind="danger"
-        />
-      </ComboButton>
+      <ComboButton
+        size="sm"
+        label="Payload"
+        onClick={generatePostPayload}
+      ></ComboButton>
 
       {isModalOpen && (
         <ClosableModal
@@ -112,47 +112,24 @@ executePipeline(); // it will execute the pipeline....
             zIndex: "9999",
           }}
         >
-          <div style={{ marginBottom: "5px" }}>
-            <button
-              onClick={() => setActiveTab("js")}
-              style={{
-                marginRight: "10px",
-                backgroundColor: activeTab === "js" ? "#666" : "#444",
-                color: "#fff",
-                border: "none",
-                padding: "10px 15px 10px 10px",
-                cursor: "pointer",
-                transition: "background-color 0.3s",
-              }}
-            >
-              JavaScript
-            </button>
-            <button
-              onClick={() => setActiveTab("python")}
-              style={{
-                backgroundColor: activeTab === "python" ? "#666" : "#444",
-                color: "#fff",
-                border: "none",
-                padding: "10px 15px 10px 10px",
-                cursor: "pointer",
-                transition: "background-color 0.3s",
-              }}
-            >
-              Python
-            </button>
-          </div>
-          <div>
-            {activeTab === "js" && (
-              <CodeSnippet type="multi" feedback="Copied to clipboard">
-                {codePayload.jsCode}
-              </CodeSnippet>
-            )}
-            {activeTab === "python" && (
-              <CodeSnippet type="multi" feedback="Copied to clipboard">
-                {codePayload.pythonCode}
-              </CodeSnippet>
-            )}
-          </div>
+          <Tabs>
+            <TabList aria-label="Code Tabs" contained>
+              <Tab>JavaScript</Tab>
+              <Tab>Python</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <CodeSnippet type="multi" feedback="Copied to clipboard">
+                  {codePayload.jsCode}
+                </CodeSnippet>
+              </TabPanel>
+              <TabPanel>
+                <CodeSnippet type="multi" feedback="Copied to clipboard">
+                  {codePayload.pythonCode}
+                </CodeSnippet>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </ClosableModal>
       )}
     </>
