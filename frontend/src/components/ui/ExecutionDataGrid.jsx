ad437f88a2fb2d70542da77e2a965b03b9a50f4d
Jon Magoon <js.magoon@gmail.com>
2024-11-04 10:55:03 -0700

    fix unused import, fix missing bucketname

--- frontend/src/components/ui/ExecutionDataGrid.jsx
+++ frontend/src/components/ui/ExecutionDataGrid.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo } from "react";
 import ClosableModal from "./modal/ClosableModal";
 import { lineageAtom } from "@/atoms/pipelineAtom";
 import { useAtom } from "jotai";
