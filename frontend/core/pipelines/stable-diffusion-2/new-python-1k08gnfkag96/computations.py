5ff32201a14510f58d31fe256c6969de594e5171
unknown <meet@zetane.com>
2024-11-15 11:10:13 -0500

    stable diffusion v2

---
+++ frontend/core/pipelines/stable-diffusion-2/new-python-1k08gnfkag96/computations.py
@@ -0,0 +1,29 @@
+def compute(prompt, inference_steps):
+    """
+    prompt: text description of image
+    inference_steps: difussion steps
+
+    output: generated image path
+
+    use GPU to run this pipeline, we are using float16 dtype
+    """
+    from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+    import torch 
+    model_id = "stabilityai/stable-diffusion-2"
+    
+    # Use the Euler scheduler here instead
+    scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+    pipe = pipe.to("cuda")
+    
+    image = pipe(prompt, num_inference_steps=inference_steps).images[0]
+        
+    image.save("result.png")
+
+    return {"generated_image_path": "result.png"}
+
+
+def test():
+    """Test the compute function."""
+
+    print("Running test")
