815292a6b52334f934ba236b695b7f143df44b9f
Jon Magoon <js.magoon@gmail.com>
2024-12-05 08:40:24 -0700

    fix anvil backwards compatibility (#270)
    * fix anvil backwards compatibility
    
    * capitalization

--- frontend/server/git.js
+++ frontend/server/git.js
@@ -32,7 +32,7 @@ export async function ensureGitRepoAndCommitBlocks(
     await Promise.all(
       buildContextStatuses
         .filter((context) => {
-          return context.hash != "";
+          return context.s3Key != "" || (context?.hash && context.hash != "");
         })
         .map(async ({ blockKey }) => {
           const sourcePath = path.join(buildPath, blockKey);
